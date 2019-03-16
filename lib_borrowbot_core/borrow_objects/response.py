6f088306f08795b795e2e47684f6129ac1cb00b6
frankwang95 <fkwang@uchicago.edu>
2019-03-16 11:18:17 -0700

    Some attribute fixes.

--- lib_borrowbot_core/borrow_objects/response.py
+++ lib_borrowbot_core/borrow_objects/response.py
@@ -13,9 +13,11 @@ class LoanResponse(object):
 
 
     def init_from_keyval_type(self, keyval):
-        self.loan_id = keyval['loan_id']
+        self.response_id = keyval['response_id']
         self.retrieval_datetime = keyval.get('retrieval_datetime', datetime.utcnow())
         self.amount = keyval['amount']
+        self.repaid = keyval['repaid']
         self.lender_id = keyval['lender_id']
         self.borrower_id = keyval['borrower_id']
+        self.source_submission_id = keyval['source_comment_id']
         self.source_comment_id = keyval['source_comment_id']
